9b4f3b16bd9064eb32e4cad7d36c24c5b26493cf
SigneThrane <signecathrane@gmail.com>
2022-09-15 11:05:08 +0200

    1

---
+++ page-members.php
@@ -0,0 +1,21 @@
+<h1><br> 
+				BLIV MEDLEM</h1> 
+
+				<p>Du har mulighed for at blive medlem, og få 10% rabat i vores hyggelige områder, både ude og inde.</p>	
+			
+
+		<div class="medlemskab">	
+				
+				<div class="contactform">
+					<div class="hero">
+					<img id="icon" width="50%" src="<?php the_field('medlems_kort'); ?>"/>
+						
+								<h3>FÅ ET MEDLEMSKORT TIL ESBJERG <br>
+									STREET FOOD</h3>
+					</div>
+					
+					<p><?php echo apply_shortcodes( '[contact-form-7 id="98" title="Bliv medlem"]' ); ?></p>
+				</div>
+		</div>
+
+	</main>
